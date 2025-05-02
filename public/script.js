5fd3f60896378e148053347d426b12c9914ce4ce
blueEarbudsCamelCase <meridith.snair28@na.oneschoolglobal.com>
2025-05-02 00:54:40 +0000

    PiP debugging script

--- public/script.js
+++ public/script.js
@@ -990,8 +990,8 @@ const ctx = canvas.getContext('2d');
 ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 
 // Set the CSS size of the canvas
-canvas.style.width = `${displayWidth}`;
-canvas.style.height = `${displayHeight}`;
+canvas.style.width = `${displayWidth}px`;
+canvas.style.height = `${displayHeight}px`;
 
 // Create a video element for PiP
 const videoElement = document.createElement('video');
